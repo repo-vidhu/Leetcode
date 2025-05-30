738b401dc1319424acb761b65113d4a9dfa3c372
repo-vidhu <vidhurams2003@gmail.com>
2025-05-30 08:31:19 +0530

    Add files via upload

---
+++ hR_5.py
@@ -0,0 +1,57 @@
+# Staircase detail
+
+# This is a staircase of size :
+
+#    #
+#   ##
+#  ###
+# ####
+# Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+# Write a program that prints a staircase of size .
+
+# Function Description
+
+# Complete the  function with the following parameter(s):
+
+# : an integer
+# Print
+
+# Print a staircase as described above. No value should be returned.
+# Note: The last line is not preceded by spaces. All lines are right-aligned.
+
+# Input Format
+
+# A single integer, , denoting the size of the staircase.
+
+# Constraints
+
+#  .
+
+# Sample Input
+
+# 6 
+# Sample Output
+
+#      #
+#     ##
+#    ###
+#   ####
+#  #####
+# ######
+# Explanation
+
+# The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
+
+def staircase(n):
+    sp=n-1
+    hs=1
+    for i in range(n):
+        print(" "*sp,"#"*hs,sep="")
+        sp-=1
+        hs+=1
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
