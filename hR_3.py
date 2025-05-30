9245c704aa07f3cbcd8189cd2ec47d91475412a9
repo-vidhu <vidhurams2003@gmail.com>
2025-05-30 08:11:58 +0530

    Add files via upload

---
+++ hR_3.py
@@ -0,0 +1,77 @@
+# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+# For example, the square matrix  is shown below:
+
+# 1 2 3
+# 4 5 6
+# 9 8 9  
+# The left-to-right diagonal = .
+# The right-to-left diagonal = .
+# Their absolute difference is .
+
+# Function description
+
+# Complete the  function with the following parameter:
+
+# : a 2-D array of integers
+# Return
+
+# : the absolute difference in sums along the diagonals
+# Input Format
+
+# The first line contains a single integer, , the number of rows and columns in the square matrix .
+# Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+# Constraints
+
+# Sample Input
+
+# STDIN      Function
+# -----      --------
+# 3           arr[][] sizes n = 3, m = 3
+# 11 2 4     arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+# 4 5 6
+# 10 8 -12
+# Sample Output
+
+# 15
+# Explanation
+
+# The primary diagonal is:
+
+# 11
+#    5
+#      -12
+# Sum across the primary diagonal: .
+
+# The secondary diagonal is:
+
+#      4
+#    5
+# 10
+# Sum across the secondary diagonal: 
+# Difference: 
+
+import os
+def diagonalDifference(arr,n):
+    x=0
+    y=0
+    for i in range (n):
+        x+=arr[i][i]
+        y+=arr[i][n-i-1] 
+    return abs(x-y)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+    print(n)
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr,n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
